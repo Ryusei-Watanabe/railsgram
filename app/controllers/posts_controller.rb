dd8baa3fd4b259f9fe7e250d15ec53da1f59f041
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-08-26 20:20:42 +0900

    confirm.error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,30 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
   end
-
-  # GET /posts/1
-  # GET /posts/1.json
   def show
   end
-
-  # GET /posts/new
-  def new
-    @post = Post.new
+  def confirm
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    render :new if @post.invalid?
   end
-
-  # GET /posts/1/edit
   def edit
   end
-
-  # POST /posts
-  # POST /posts.json
+  def new
+    if params[:back]
+      @post = Post.new(post_params)
+    else
+      @post = Post.new
+    end
+  end
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -36,9 +32,6 @@ class PostsController < ApplicationController
       end
     end
   end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -50,9 +43,6 @@ class PostsController < ApplicationController
       end
     end
   end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -62,13 +52,10 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
-
-    # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:image, :content)
+      params.require(:post).permit(:image, :image_cache, :content)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  resources :posts
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
-  
+
 end
