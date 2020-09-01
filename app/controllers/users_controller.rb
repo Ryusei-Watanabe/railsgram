71688eef2faad92cf4ad09932f43e31cde3fa10d
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-09-01 18:59:47 +0900

    heroku deproy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :destroy]
-  before_action :authenticate_user
   def new
   @user = User.new
   end
